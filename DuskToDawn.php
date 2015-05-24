46af09a270dee47a4c4d5a3032df2cd1fdbc71c3
Isarra <zhorishna@gmail.com>
2015-05-24 09:36:51 +0000

    Convert extension registration to use skin.json
    Change-Id: Ie8db78f2bc152b1c1c8516c5f5ee7e9e368d2e30

--- DuskToDawn.php
+++ DuskToDawn.php
@@ -12,40 +12,18 @@
  *
  * To install, place the DuskToDawn folder (the folder containing this file!) into
  * skins/ and add this line to your wiki's LocalSettings.php:
- * require_once("$IP/skins/DuskToDawn/DuskToDawn.php");
+ * wfLoadSkin( 'DuskToDawn' );
  */
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'Not a valid entry point.' );
+if ( function_exists( 'wfLoadSkin' ) ) {
+	wfLoadSkin( 'DuskToDawn' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['DuskToDawn'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for DuskToDawn skin. Please use wfLoadSkin instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the DuskToDawn skin requires MediaWiki 1.25+' );
 }
-
-// Skin credits that will show up on Special:Version
-$wgExtensionCredits['skin'][] = array(
-	'path' => __FILE__,
-	'name' => 'Dusk To Dawn',
-	'version' => '1.3.1',
-	'author' => array( '[http://automattic.com/ Automattic]', 'Jack Phoenix' ),
-	'description' => 'A dark theme that melds old-style organic ornaments with modern design and typography',
-	'url' => 'https://www.mediawiki.org/wiki/Skin:DuskToDawn',
-);
-
-// The first instance must be strtolower()ed so that useskin=dusktodawn works and
-// so that it does *not* force an initial capital (i.e. we do NOT want
-// useskin=Dusktodawn) and the second instance is used to determine the name of
-// *this* file.
-$wgValidSkinNames['dusktodawn'] = 'DuskToDawn';
-
-// Autoload the skin class, set up i18n, set up CSS & JS (via ResourceLoader)
-$wgAutoloadClasses['SkinDuskToDawn'] = __DIR__ . '/DuskToDawn.skin.php';
-$wgMessagesDirs['SkinDuskToDawn'] = __DIR__ . '/i18n';
-
-$wgResourceModules['skins.dusktodawn'] = array(
-	'styles' => array(
-		// Styles custom to the DuskToDawn skin
-		'skins/DuskToDawn/resources/style.css' => array( 'media' => 'screen' )
-	),
-	'scripts' => array(
-		'/skins/DuskToDawn/resources/js/audio.js'
-	),
-	'position' => 'top'
-);
